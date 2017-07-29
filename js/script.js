c7ba0b177bc610fbe4140ff1bd3f5817c604a96a
mateuszrosa <mateusz.rosa@gmail.com>
2017-07-29 14:50:16 +0200

    inputs as buttons

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
 document.addEventListener('DOMContentLoaded',function() {
 
   let numbers = document.querySelectorAll('.numbers');
-  let input = document.querySelector('.result input');
-  console.log(input);
+  let input = document.querySelector('.screen input');
+  let clear = document.querySelector('.clear');
+  let sum = document.querySelector('.sum');
+  let result = document.querySelector('.result');
+  console.log(result);
 
 for(let i=0; i<numbers.length; i++) {
   numbers[i].addEventListener('click', function() {
-    console.log(numbers[i].innerText);
-    input.value += numbers[i].innerText;
+    var a = input.value += numbers[i].innerText;
+    console.log(a);
   })
 }
+clear.addEventListener('click', function() {
+  input.value = input.value.slice(0, -1);
+})
+
+sum.addEventListener('click', function() {
+  input.value += '+';
+})
+
+result.addEventListener('click', function() {
+  console.log(a);
+  input.value = a;
+})
 
 })

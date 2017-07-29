89d8fc61cb48bf88035f56e0f212b04cce02ee3f
mateuszrosa <mateusz.rosa@gmail.com>
2017-07-29 11:43:03 +0200

    inputs script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded',function() {
-  console.log('work');
+
+  let buttons = document.querySelectorAll('.button');
+  let input = document.querySelector('.result input');
+  console.log(input);
+
+for(let i=0; i<buttons.length; i++) {
+  buttons[i].addEventListener('click', function() {
+    console.log(buttons[i].innerText);
+    input.value = buttons[i].innerText;
+  })
+}
+
 })
